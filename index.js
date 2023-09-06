9d92d63fa06e8b3629ffd9ab25e9c5c5ee12d436
misafxd <pisichu.mafs@gmail.com>
2023-09-06 12:16:25 -0600

    Modify buttons class list
    Add class num, clear and op to different types op button functions

--- index.js
+++ index.js
@@ -1,36 +1,85 @@
+const num = document.querySelectorAll('.btn.num');
+const operator = document.querySelectorAll('.btn.op');
+const display = document.querySelector('.display-value');
+const clear = document.querySelector('.btn.clear');
+
 function add(a,b){
-    return a + b;
+    return +a + +b;
 }
 
 function subtract(a,b){
-    return a - b ;
+    return +a - +b ;
 }
 
 function multiply(a,b){
-    return a * b;
+    return +a * +b;
 }
 
 function divide(a,b){
-    if(b==0){
+    if(+b == 0){
         return "ERROR"
     }else {
-        return a/b;
+        return +a / +b;
     }
 }
 
 function operate(op, num1, num2){
+    let operationResult;
    switch(op){
-    case '+': add(num1,num2);
+    case '+': operationResult = add(num1,num2);
     break;
-    case '-': subtract(num1,num2);
+    case '-': operationResult = subtract(num1,num2);
     break;
-    case '/': divide(num1,num2);
+    case '/': operationResult = divide(num1,num2);
     break;
-    case '*': multiply(num1,num2);
+    case '*': operationResult = multiply(num1,num2);
     break;
    }
+   return operationResult;
 }
 
 let num1;
-let num2;
+let num2='';
 let op;
+let result;
+
+
+num.forEach(button => {
+    button.addEventListener('click', () => {
+        if(!num1){
+            display.textContent += button.textContent;
+        } else{
+            num2 += button.textContent;
+            display.textContent = num2;
+        }
+    })
+})
+
+operator.forEach(button => {
+    button.addEventListener('click', () => {
+        if(!num1){
+            op= button.textContent;
+            num1= display.textContent;
+        } else if(num1 && num2){
+            result = operate(op,num1,num2);
+            num1 = result;
+            display.textContent = result;
+            num2 = ''
+        } else if(op){
+            if(op == '='){
+
+            }else{
+                op = button.textContent;
+            }
+        }
+    })
+})
+
+clear.addEventListener('click', () => {
+    display.textContent = ''
+    num1 = '';
+    num2 = '';
+    op = '';
+});
+
+
