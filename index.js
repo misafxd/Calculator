a5e7d3ff75048c1c19119e1403847d01d20f7fcc
misafxd <pisichu.mafs@gmail.com>
2023-09-05 18:01:09 -0600

    Add function operate

--- index.js
+++ index.js
@@ -11,7 +11,24 @@ function multiply(a,b){
 }
 
 function divide(a,b){
-    return a/b;
+    if(b==0){
+        return "ERROR"
+    }else {
+        return a/b;
+    }
+}
+
+function operate(op, num1, num2){
+   switch(op){
+    case '+': add(num1,num2);
+    break;
+    case '-': subtract(num1,num2);
+    break;
+    case '/': divide(num1,num2);
+    break;
+    case '*': multiply(num1,num2);
+    break;
+   }
 }
 
 let num1;
